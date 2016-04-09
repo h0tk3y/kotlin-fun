94f15a3a898bbbdc1e8410f17a011ae0cf3dbe90
Sergey Igushkin <hotkeytlt@gmail.com>
2016-04-09 03:56:48 +0300

    Synchronization & nullability.

--- src/main/kotlin/FieldProperty.kt
+++ src/main/kotlin/FieldProperty.kt
@@ -1,14 +1,13 @@
+
 import com.github.h0tk3y.kotlinFun.util.WeakIdentityHashMap
-import java.util.*
 import kotlin.reflect.KProperty
 
 /**
  * Created by igushs on 4/9/16.
  */
 
-class FieldProperty<R, T : Any>(val synchronized: Boolean = false,
-                                val initializer: (R) -> T = { throw IllegalStateException("Not initialized.") }) {
-    private val map = WeakIdentityHashMap<R, T>().let { if (synchronized) Collections.synchronizedMap(it) else it }
+class FieldProperty<R, T : Any>(val initializer: (R) -> T = { throw IllegalStateException("Not initialized.") }) {
+    private val map = WeakIdentityHashMap<R, T>()
 
     operator fun getValue(thisRef: R, property: KProperty<*>): T =
             map[thisRef] ?: setValue(thisRef, property, initializer(thisRef))
@@ -19,15 +18,44 @@ class FieldProperty<R, T : Any>(val synchronized: Boolean = false,
     }
 }
 
-class NullableFieldProperty<R, T>(val synchronized: Boolean = false,
-                                  val initializer: (R) -> T? = { null }) {
-    private val map = WeakIdentityHashMap<R, T>().let { if (synchronized) Collections.synchronizedMap(it) else it }
+class NullableFieldProperty<R, T>(val initializer: (R) -> T? = { null }) {
+    private val map = WeakIdentityHashMap<R, T>()
 
     operator fun getValue(thisRef: R, property: KProperty<*>): T? =
-            map[thisRef] ?: setValue(thisRef, property, initializer(thisRef))
+            if (thisRef in map) map[thisRef] else setValue(thisRef, property, initializer(thisRef))
 
     operator fun setValue(thisRef: R, property: KProperty<*>, value: T?): T? {
         map[thisRef] = value
         return value
     }
+}
+
+class SynchronizedSafeFieldProperty<R, T : Any>(val initializer: (R) -> T = { throw IllegalStateException("Not initialized.") }) {
+    private val map = WeakIdentityHashMap<R, T>()
+
+    operator fun getValue(thisRef: R, property: KProperty<*>): T = synchronized(map) {
+        map[thisRef] ?: setValue(thisRef, property, initializer(thisRef))
+    }
+
+    private fun set(thisRef: R, value: T): T {
+        map[thisRef] = value
+        return value
+    }
+
+    operator fun setValue(thisRef: R, property: KProperty<*>, value: T): T = synchronized(map) { set(thisRef, value) }
+}
+
+class SynchronizedNullableFieldProperty<R, T>(val initializer: (R) -> T? = { null }) {
+    private val map = WeakIdentityHashMap<R, T>()
+
+    operator fun getValue(thisRef: R, property: KProperty<*>): T? = synchronized(map) {
+        if (thisRef in map) map[thisRef] else set(thisRef, initializer(thisRef))
+    }
+
+    private fun set(thisRef: R, value: T?): T? {
+        map[thisRef] = value
+        return value
+    }
+
+    operator fun setValue(thisRef: R, property: KProperty<*>, value: T?): T? = synchronized(map) { set(thisRef, value) }
 }
